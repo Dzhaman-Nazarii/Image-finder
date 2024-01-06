71732b897a5ccb8f5b1af1ae5e35e47ed9c4f223
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2024-01-06 09:43:15 +0200

    added start files

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Searchbar from "components/searchbar/Searchbar";
+
+class App extends Component {
+  render() {
+    return (
+      <Searchbar/>
+    )
+  }
+}
+
+export default App
